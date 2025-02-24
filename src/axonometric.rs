6861c7c255acf7ee0546500a61b33ef3d14dc583
UnrelatedString <unrelatedstring@gmail.com>
2025-02-24 14:24:30 -0500

    and ofc it just. doesn't need that column

--- src/axonometric.rs
+++ src/axonometric.rs
@@ -19,9 +19,9 @@ impl Octant {
     pub fn project(self, coords: Vec3A) -> Vec2 {
         // because APPARENTLY I can't use sqrt in const...
         let PROJECTION: Mat3A = Mat3A::from_cols_array(&[
-            3.0_f32.sqrt(), 1.0,  2.0_f32.sqrt(),
-            0.0,            2.0, -2.0_f32.sqrt(),
-           -3.0_f32.sqrt(), 1.0,  2.0_f32.sqrt(),
+            3.0_f32.sqrt(), 1.0,  0.0,
+            0.0,            2.0, 0.0,
+           -3.0_f32.sqrt(), 1.0,  0.0,
        ]) / 6.0_f32.sqrt();
         // TODO: actually. use the octant lol
         let transformed = PROJECTION * coords;
