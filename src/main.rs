1934782c464f7fc15ef63a5c36ad3b8c2223061e
UnrelatedString <unrelatedstring@gmail.com>
2025-02-28 10:39:06 -0500

    it compiles. :P

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,63 @@
-
-use functor_derive::Functor;
-
-mod util;
-use crate::util::{
-    Quad,
-    Drawable,
+use three_d::core::prelude::*;
+use three_d::{
+    Window,
+    Camera,
+    CpuMesh,
+    Mesh,
+    Gm,
+    ColorMaterial,
+    Srgba,
+    ClearState,
 };
 
+// mod util;
+// use crate::util::{
+//     Quad,
+//     Drawable,
+// };
+
+// what if. Mystic Eyes of Depth Perception
+
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+pub async fn run() {
+    let window = Window::new(three_d::WindowSettings {
+        title: "Eyes of Hell".to_string(),
+        min_size: (1280, 720),
+        ..Default::default()
+    }).unwrap();
+
+    // TODO: keybinds
+
+    let camera = Camera::new_orthographic(
+        // ooooh what if I make the sides of things tinted by the left and right eyes
+        window.viewport(),
+        Vec3::new(20.0, 20.0, 20.0), // just trying stuff to see what actually. does the stuff I want at all
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        0.0,
+        40.0,
+    );
+
+    let cube = Gm::new(
+        Mesh::new(&window.gl(), &CpuMesh::cube()),
+        ColorMaterial {
+            color: Srgba::BLUE,
+            ..Default::default()
+        },
+    );
+
+    window.render_loop (move |frame_input| {
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.0, 0.0, 0.0, 1.0, 1.0))
+            .render(&camera, &[&cube], &[]);
+
+            Default::default()
+    });
+}
--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,8 @@
-use macroquad::prelude::*;
+use three_d::renderer::Camera;
 use functor_derive::Functor;
-
 use std::f32::consts::PI;
 use std::ops;
 pub use Sign::*;
-
-
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Sign {
@@ -40,23 +37,28 @@ pub struct Octant {
     pub z: Sign,
 }
 
-impl Octant {
-    pub fn projection(self) -> Mat3A {
-        let alpha = self.y * (PI / 6.0).tan().asin();
-        let beta = match self {
-            Octant{x: Pos, z: Pos, ..} => 3.0,
-            Octant{x: Neg, z: Pos, ..} => 5.0,
-            Octant{x: Neg, z: Neg, ..} => 7.0,
-            Octant{x: Pos, z: Neg, ..} => 1.0,
-        };
-        Mat3A::from_euler(EulerRot::ZXY, 0.0, alpha, beta * PI / 4.0)
-    }
+// impl Octant {
+//     pub fn projection(self) -> Mat3A {
+//         let alpha = self.y * (PI / 6.0).tan().asin();
+//         let beta = match self {
+//             Octant{x: Pos, z: Pos, ..} => 3.0,
+//             Octant{x: Neg, z: Pos, ..} => 5.0,
+//             Octant{x: Neg, z: Neg, ..} => 7.0,
+//             Octant{x: Pos, z: Neg, ..} => 1.0,
+//         };
+//         Mat3A::from_euler(EulerRot::ZXY, 0.0, alpha, beta * PI / 4.0)
+//     }
 
-    pub fn project(self, coords: Vec3A) -> Vec2 {
-        let transformed = self.projection() * coords;
-        Vec2::new(
-            transformed.x,
-            transformed.y,
-        )
-    }
+//     pub fn project(self, coords: Vec3A) -> Vec2 {
+//         let transformed = self.projection() * coords;
+//         Vec2::new(
+//             transformed.x,
+//             transformed.y,
+//         )
+//     }
+// }
+
+// 
+pub trait Region {
+    
 }
