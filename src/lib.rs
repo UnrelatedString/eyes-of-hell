8f987b83f43974ba677629394342de85c7a65c1d
UnrelatedString <unrelatedstring@gmail.com>
2025-03-03 14:50:27 -0500

    Okay. that was. quite informative

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
 mod meat;
-use meat::run;
 
 #[cfg(target_family = "wasm")]
+mod inner {
+
 use wasm_bindgen::prelude::*;
+use web_sys::console::log_1;
 
-#[cfg(target_family = "wasm")]
+use crate::meat::run;
+use three_d::WindowSettings;
+
 #[wasm_bindgen(start)]
 pub async fn start() -> Result<(), JsValue> {
     #[cfg(feature = "web_debug")] {
+        log_1(&JsValue::from_str("WASM loaded and running!"));
         console_log::init_with_level(log::Level::Debug).unwrap();
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     }
     
-    run(None).await;
+    run(WindowSettings {
+        initial_size: None,
+        // TODO: canvas (why isn't it documented ;_; like I know it's a #[cfg] but just mention it manually?)
+        ..Default::default()
+    }).await;
     Ok(())
 }
+}
+
+#[cfg(not(target_family = "wasm"))]
+mod inner {}
+
+pub use inner::*;
