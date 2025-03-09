d6a99f973c3e1c407807dbb7819c15c42fb72515
UnrelatedString <unrelatedstring@gmail.com>
2025-03-09 03:45:23 -0400

    the commit ever

---
+++ src/meat/storage.rs
@@ -0,0 +1,5 @@
+// i like kinda want to make this its own crate but like..
+// it's kinda just stupid and scuffed and literally anyuone could do this without like
+// me having to overengineer exactly how to force them into the exact abstractions
+// i want around it an
+// d also  reflexively give escape hatches that are easier to use anwyash why am i doing this ringht now isntead of emailing my fucking engl teacher oh yeah soi i stop spending all day on purescript libraries that nobody will actually pat me on the back for writing because i get so fucking lonely doing thateven though it is like really satisfying to just be programming something and feel in control and 
