05c812f69e1e3d4e4273fbc47174a9e6346d3021
UnrelatedString <unrelatedstring@gmail.com>
2025-03-10 20:50:30 -0400

    wait does localstorage seriously only take strings
    I was so looking forward to using serde-wasm-bindgen :(
    
    well whatever if it did accept arbitrary JSValues then they'd probably get serialized as *shudders* JSON so this just means I get to use a more memory-efficient format >:3

--- src/meat/storage.rs
+++ src/meat/storage.rs
@@ -1,10 +1,18 @@
 use serde::{ Serialize, Deserialize }
 
-pub fn save<T: Serialize>(value: &T, name: &str) -> () {
+pub enum PersistentError {
+    pub DeserializationError(String),
+    pub FileError(String),
+    pub NoWindow,
+    pub NoLocalStorage,
+    pub JsError(String),
+}
+
+pub fn save<T: Serialize>(value: &T, name: &str) -> Result<(), PersistentError> {
     platform_specific::save(value, name)
 }
 
-pub fn load<T: Deserialize>(name: &str) -> T {
+pub fn load<T: Deserialize>(name: &str) -> Result<T, PersistentError> {
     platform_specific::load(value, name)
 }
 
@@ -13,23 +21,36 @@ mod platform_specific {
 
     use std::io;
     use std::fs::File;
+    use serde_yml::{ from_reader, to_writer };
 
-    pub fn save<T: Serialize>(value: &T, name: &str) -> () {
+    pub fn save<T: Serialize>(value: &T, name: &str) -> Result<(), PersistentError> {
         
     }
     
-    pub fn load<T: Deserialize>(name: &str) -> T {
+    pub fn load<T: Deserialize>(name: &str) -> Result<T, PersistentError> {
         
     }
 }
 
 #[cfg(target_family = "wasm")]
 mod platform_specific {
-    pub fn save<T: Serialize>(value: &T, name: &str) -> () {
-        
+
+    use web_sys::{ window, Storage };
+    use serde_wasm_bindgen::{ from_value, to_value }
+
+    fn local_storage() -> Result<, PersistentError> {
+        window()
+            .ok_or(NoWindow)?
+            .local_storage()
+            .map_err(|err| JsError(format!("{err:?}").to_owned()))?
+            .ok_or(NoLocalStorage)
     }
 
-    pub fn load<T: Deserialize>(name: &str) -> T {
+    pub fn save<T: Serialize>(value: &T, name: &str) -> Result<(), PersistentError> {
+        local_storage()?.
+    }
+
+    pub fn load<T: Deserialize>(name: &str) -> Result<T, PersistentError> {
         
     }
 }
